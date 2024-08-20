6c641606ebf3ff89eb016021b90651d5e0e7823b
MuhammadSarim7 <muhammadsaariim@gmail.com>
2024-08-20 17:04:54 +0000

    Question 2

--- Q1.py
+++ Q1.py
@@ -1,5 +1,4 @@
 currentYear = int(input("Enter current year: "))
 birthYear = int(input("Enter birth year: "))
-age: int
-age = currentYear-birthYear
+age : int = currentYear-birthYear
 print("Your age is", age)
---
+++ Q2.py
@@ -0,0 +1,4 @@
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+area : float = length * width
+print("Area of rectangle is:", area)
