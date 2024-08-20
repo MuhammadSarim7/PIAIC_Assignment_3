919eed1f3750241b98091d664ef86336743f0049
MuhammadSarim7 <muhammadsaariim@gmail.com>
2024-08-20 17:09:57 +0000

    Question 5

---
+++ Q5.py
@@ -0,0 +1,8 @@
+temperatureFarhenheit = float(input("Enter temperature in Farhenheit: "))
+temperatureCelsius : float
+temperatureCelsius = (temperatureFarhenheit -32)*5/9
+print("Temperature in Celsius",temperatureCelsius)
+temperatureCelsius = float(input("Enter temperature in Celsius: "))
+temperatureFarhenheit: float
+temperatureFarhenheit = (temperatureCelsius * 9/5)+32
+print("Temperature in Farhenheit", temperatureFarhenheit)
